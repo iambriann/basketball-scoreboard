1ac614862cf77384cc7813301a4bd8e4b08ccf7b
Brian Nguyen <brian.nguyen93@gmail.com>
2023-05-17 21:27:42 +1000

    Commit initial files

---
+++ index.js
@@ -0,0 +1,14 @@
+let home = 0
+let guest = 0
+let scorehome = document.getElementById("homedisplay")
+let scoreguest = document.getElementById("guestdisplay")
+
+function addh(x) {
+    home += x;
+    scorehome.textContent = home;
+}
+
+function addg(x) {
+    guest += x;
+    scoreguest.textContent = guest;
+}
